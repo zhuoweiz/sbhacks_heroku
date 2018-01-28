448c8003b0b7d677913d8a68667b891c35e557b6
JOY <zhuoweiz@usc.edu>
2018-01-28 03:10:47 -0800

    basic user system

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 //dependencies
-var express = require("express"),
-	bodyParser = require("body-parser"),
-	request = require("request"),
-	methodOverride = require("method-override"),
-	app = express(),
-	// $ = require("jquery"),
-	mongoose = require("mongoose");
+var bodyParser 		= require("body-parser"),
+	methodOverride  = require("method-override");
+	request 		= require("request"),
+	mongoose 		= require("mongoose"),
+	express 		= require("express"),
 
-//own files
-var notes = require("./others.js");
+	passport 		= require("passport"),
+	LocalStrategy 	= require("passport-local"),
 
+	//models
+	notes 			= require("./others.js")
+	Dm 				= require("./models/demands"),
+	Sp 				= require("./models/supply"),
+	User 			= require("./models/user"),
+	seedDB 			= require("./models/seeds");
+
+
+var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(methodOverride("_method"));
 
-//==================================== DB setup ============
+//================== DB setup ============
 mongoose.Promise = global.Promise;
 var uri = "mongodb://xianhomedroy:19980110.Zz@ds046267.mlab.com:46267/uzedb";
 mongoose.connect(uri);
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-//---------------------------- models
-var Dm = require("./models/demands");
-var Sp = require("./models/supply");
-
+//================== PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "Rusty",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate())); //use the function form user model
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //=============================== others =====
 // request('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCXgzdvX_NggJZqfYKEceahh2do7zED09c', function(err, response, body){
@@ -38,20 +51,24 @@ var Sp = require("./models/supply");
 //   	}
 // });
 
+//middleware
+app.use(function(req,res,next){
+	res.locals.currentUser = req.user;
+	next();
+});
+
 //==================================== Routes =================
 app.get("/", function(req,res){
-	//map request
-
-
-	res.render("index");
+	//special
+	res.render("index", {currentUser: req.user});
 });
 
 //---------------demand
-app.get("/demand", function(req,res){
+app.get("/demand", isLoggedIn, function(req,res){
 	res.render("demand");
 });
 
-app.post("/demanded", function(req,res){
+app.post("/demanded", isLoggedIn, function(req,res){
 	//-----------------need modification
 	Dm.create(req.body.demand, function(err, newDemand){
 		if(err){
@@ -71,11 +88,11 @@ app.get("/demanded", function(req,res){
 });
 
 //----------------supply
-app.get("/supply", function(req,res){
+app.get("/supply", isLoggedIn, function(req,res){
 	res.render("supply");
 });
 
-app.post("/supplied", function(req,res){
+app.post("/supplied", isLoggedIn, function(req,res){
 	//-----------------need modification
 	Sp.create(req.body.supply, function(err, newDemand){
 		if(err){
@@ -97,8 +114,46 @@ app.get("/supplied", function(req,res){
 // 	res.render("demand");
 // });
 
-// =========================  MAP REQUEST ==============
+// =========================  AUTH ROUTES SETUP ==============
+app.get("/signup", function(req,res){
+	res.render("signup");
+});
 
+app.post("/signup", function(req,res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("signup");
+		}
+		passport.authenticate("local")(req,res,function(){
+			console.log("register success!");
+			res.redirect("/");
+		});
+	});
+});
+
+app.get("/login", function(req,res){
+	res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",{
+	successRedirect: "/",
+	railureRedirect: "/login"
+}), function(req,res){
+});
+
+app.get("/logout", function(req,res){
+	req.logout();
+	res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 
 // =========================  OTHER ROUTES ==========
 app.get("/contact", function(req,res){
